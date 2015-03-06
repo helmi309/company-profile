ef56a4a5ac175374c3bb389ca752e801df89661b
Edi Santoso <edicyber@gmail.com>
2015-03-06 18:03:08 +0700

    Fix typo

---
+++ public/views/index.blade.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kode Soft - IT Software Engineer Consultant</title>
+    <meta name="description" content="CV Kode Soft Media Nusantara">
+
+    <!-- core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<!--/head-->
+
+<body class="homepage">
+<header id="header">
+    <nav class="navbar navbar-inverse" role="banner">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> <span class="icon-bar"></span></button>
+                <a class="navbar-brand" href="/"> Kode Soft</a></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="about">About</a></li>
+                    <li><a href="services">Services</a></li>
+                    <li><a href="portfolio">Portfolio</a></li>
+                    <li><a href="contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <!--/.container-->
+    </nav>
+    <!--/nav-->
+
+</header>
+<!--/header-->
+
+<section id="main-slider" class="no-margin">
+    <div class="carousel slide">
+        <ol class="carousel-indicators">
+            <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+            <li data-target="#main-slider" data-slide-to="1"></li>
+            <li data-target="#main-slider" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active" style="background-image: url(images/slider/bg1.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-12">
+                            <div class="carousel-content">
+                                <h1>Welcome to Official Kode Soft Site</h1>
+
+                                <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit sed diam nonummy nibh
+                                    euismod tincidunt laoreet</h2>
+                                <a class="btn-slide" href="about">Read More</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/.item-->
+
+            <div class="item" style="background-image: url(images/slider/bg2.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-12">
+                            <div class="carousel-content">
+                                <h1>IT Software Engineer Consultant</h1>
+
+                                <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit sed diam nonummy nibh
+                                    euismod tincidunt laoreet</h2>
+                                <a class="btn-slide" href="about">Read More</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/.item-->
+
+            <div class="item" style="background-image: url(images/slider/bg3.jpg)">
+                <div class="container">
+                    <div class="row slide-margin">
+                        <div class="col-sm-12">
+                            <div class="carousel-content">
+                                <h1>Built Awesome application with <i class="fa fa-heart text-danger"></i></h1>
+
+                                <h2>Lorem ipsum dolor sit amet, consectetuer adipiscing elit sed diam nonummy nibh
+                                    euismod tincidunt laoreet</h2>
+                                <a class="btn-slide" href="about">Read More</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--/.item-->
+        </div>
+        <!--/.carousel-inner-->
+    </div>
+    <!--/.carousel-->
+    <a class="prev hidden-xs" href="#main-slider" data-slide="prev"> <i class="fa fa-chevron-left"></i> </a> <a
+            class="next hidden-xs" href="#main-slider" data-slide="next"> <i class="fa fa-chevron-right"></i> </a>
+</section>
+<!--/#main-slider-->
+<div class="color-border"></div>
+<section id="feature">
+    <div class="container">
+        <div class="center">
+            <h2>Featured Services</h2>
+
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros tristique
+                bibendum. Nunc quis semper sem.<br>
+                Morbi libero elit elementum quis faucibus ac, condimentum quis nibh.</p>
+        </div>
+        <div class="row">
+            <div class="features">
+                <div class="col-md-4 col-sm-6">
+                    <div class="feature-wrap"><i class="fa fa-desktop"></i>
+
+                        <h2>Web Development</h2>
+
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros
+                            tristique.</h3>
+                    </div>
+                </div>
+                <!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="feature-wrap"><i class="fa fa-cogs"></i>
+
+                        <h2>App Development</h2>
+
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros
+                            tristique.</h3>
+                    </div>
+                </div>
+                <!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6">
+                    <div class="feature-wrap"><i class="fa fa-rocket"></i>
+
+                        <h2>Brand & Identity</h2>
+
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros
+                            tristique.</h3>
+                    </div>
+                </div>
+                <!--/.col-md-4-->
+            </div>
+            <!--/.services-->
+        </div>
+        <!--/.row-->
+    </div>
+    <!--/.container-->
+</section>
+<!--/#feature-->
+
+<section id="testimonials" class="service-item">
+    <div class="container">
+        <div class="center">
+            <h2>What Our Clients Says</h2>
+
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros tristique
+                bibendum. Nunc quis semper sem.<br>
+                Morbi libero elit elementum quis faucibus ac, condimentum quis nibh.</p>
+        </div>
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row testimonials">
+                    <div class="col-sm-4">
+                        <blockquote>
+                            <p class="clients-words">Morbi eget enim nisi. In venenatis porta est ut molestie nulla
+                                facilisi. Aliquam vehicula efficitur lorem eget eleifend.</p>
+                            <span class="clients-name">- John Doe, Company</span> <img class="img-circle img-thumbnail"
+                                                                                       src="images/testimonials/man1.jpg">
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-4">
+                        <blockquote>
+                            <p class="clients-words">Maecenas tincidunt venenatis tempus. Nulla tristique euismod urna
+                                at ullamcorper.</p>
+                            <span class="clients-name">- Jane Doe, Company</span> <img class="img-circle img-thumbnail"
+                                                                                       src="images/testimonials/woman1.jpg">
+                        </blockquote>
+                    </div>
+                    <div class="col-sm-4">
+                        <blockquote>
+                            <p class="clients-words">Aliquam euismod nunc id eros sollicitudin interdum. Integer eget
+                                sem nec nulla dictum dictum.</p>
+                            <span class="clients-name">- Brian Smith, Company</span> <img
+                                    class="img-circle img-thumbnail" src="images/testimonials/man1.jpg"></blockquote>
+                    </div>
+                </div>
+                <!--/.row-->
+            </div>
+            <!--/.col-->
+        </div>
+        <!--/.row-->
+    </div>
+    <!--/.container-->
+</section>
+<!--/#testimonials-->
+
+<section id="middle">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <h3>Some Facts About Us</h3>
+                <br>
+
+                <div class="media">
+                    <i class="fa fa-smile-o pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"> Laravel PHP Framework</h3>
+                        Etiam eget nisl vel justo suscipit ullamcorper sed at lacus.
+                    </div>
+                </div>
+                <br>
+
+                <div class="media"><i class="fa fa-check-square-o pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"> AngularJs </h3>
+                        Sed scelerisque nunc quis est efficitur dictum vivamus commodo.
+                    </div>
+                </div>
+                <br>
+
+                <div class="media"><i class="fa fa-trophy pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"> Digital Ocean </h3>
+                        Pellentesque hendrerit arcu luctus facilisis erat consectetur.
+                    </div>
+                </div>
+            </div>
+            <!--/.col-sm-6-->
+
+            <div class="col-sm-6">
+                <h3>Awesome and Modern Tools</h3>
+                <br>
+
+                <div class="media"><i class="fa fa-github-square pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"><a href="https://github.com/">GitHub.com</a></h3>
+                        Etiam eget nisl vel justo suscipit ullamcorper sed at lacus.
+                    </div>
+                </div>
+                <br>
+
+                <div class="media"><i class="fa fa-check-square-o pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"> #Slack </h3>
+                        Sed scelerisque nunc quis est efficitur dictum vivamus commodo.
+                    </div>
+                </div>
+                <br>
+
+                <div class="media"><i class="fa fa-trophy pull-left fa-3x"></i>
+
+                    <div class="media-body">
+                        <h3 class="media-heading"> Dploy.io </h3>
+                        Pellentesque hendrerit arcu luctus facilisis erat consectetur.
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!--/.row-->
+    </div>
+    <!--/.container-->
+</section>
+<!--/#middle-->
+
+<section id="partner">
+    <div class="container">
+        <div class="center">
+            <h2>Our Awesome Clients</h2>
+
+            <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non odio sed eros tristique
+                bibendum. Nunc quis semper sem.<br>
+                Morbi libero elit elementum quis faucibus ac, condimentum quis nibh.</p>
+        </div>
+        <div class="partners">
+            <ul>
+                <li><a href="#"><img class="img-responsive" src="images/partners/partner1.png"></a></li>
+                <li><a href="#"><img class="img-responsive" src="images/partners/partner2.png"></a></li>
+                <li><a href="#"><img class="img-responsive" src="images/partners/partner3.png"></a></li>
+                <li><a href="#"><img class="img-responsive" src="images/partners/partner4.png"></a></li>
+                <li><a href="#"><img class="img-responsive" src="images/partners/partner5.png"></a></li>
+            </ul>
+        </div>
+    </div>
+    <!--/.container-->
+</section>
+<!--/#partner-->
+
+<section id="get-started">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h2>Want to discuss a project?</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget nisl vel justo suscipit
+                    ullamcorper sed at lacus.<br>
+                    Aliquam euismod nunc id eros sollicitudin interdum.</p>
+                <a href="contact" class="btn btn-primary">Contact us</a></div>
+        </div>
+    </div>
+    <!--/.container-->
+</section>
+<!--/#get-started-->
+
+<div class="color-border"></div>
+<footer id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">Copyright &copy; 2015 Kode Soft Design by
+                <a href="http://www.templategarden.com" rel="nofollow">TemplateGarden</a>
+            </div>
+            <div class="col-sm-6">
+                <div class="follow-us">
+                    <a class="fa fa-facebook social-icon" href="https://facebook.com/cyberid41"></a>
+                    <a class="fa fa-twitter social-icon" href="https://twitter.com/cyberid41"></a>
+                    <a class="fa fa-linkedin social-icon" href="https://id.linkedin.com/in/cyberid41"></a>
+                    <a class="fa fa-google-plus social-icon" href="https://plus.google.com/u/0/+EdiSantosoID"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<!--/#footer-->
+
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.prettyPhoto.js"></script>
+<script src="js/jquery.isotope.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
